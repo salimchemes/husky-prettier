b5dd9a12e235b42ef3d428377a6f596cd85533ff
Salim Chemes <salim@mobomo.com>
2020-11-15 12:13:46 -0300

    testing test hook

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -18,18 +18,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'husky-prettier'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('husky-prettier');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('husky-prettier app is running!');
+    // expect(app).toBeFalsy();
   });
 });
