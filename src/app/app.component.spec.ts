5441034edd015cfddb121d852152873379049139
Salim Chemes <salim@mobomo.com>
2020-11-23 11:34:14 -0300

    commit with failing tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,6 +14,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
-    expect(app).toBeFalsy();
   });
 });
